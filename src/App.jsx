a2c3521218866078c56b384029fc264f3174c62b
arthurcorreia <euarthurcorreia@gmail.com>
2026-01-22 13:12:16 -0400

    Instalado tailwind e axios

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
   return (
     <>
       <div>
+        <h1 className="text-4xl font-bold text-blue-600">
+          Hello Tailwind!
+        </h1>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
