be6949734541adf6fa67346842f859f2fd727b6a
Arthur Correia <euarthurcorreia@gmail.com>
2026-02-02 22:44:32 -0400

    refactor: incrementando rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,35 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { Cabecalho } from './componentes/layout/Cabecalho'
 import { SecaoHero } from './componentes/secoes/SecaoHero'
 import { SecaoRecurso } from './componentes/secoes/SecaoRecurso'
 import { SecaoSobre } from './componentes/secoes/SecaoSobre'
 import { Rodape } from './componentes/layout/Rodape'
+import FormularioTutor from './componentes/formularios/FormularioTutor'
 
 function App() {
-
   return (
-    <main className="min-h-screen bg-background">
-      <Cabecalho/>
-      <SecaoHero />
-      <SecaoRecurso />
-      <SecaoSobre />
-      <Rodape />
-    </main>
+    <BrowserRouter>
+      <Routes>
+        {/* Página principal */}
+        <Route
+          path="/"
+          element={
+            <main className="min-h-screen bg-background">
+              <Cabecalho />
+              <SecaoHero />
+              <SecaoRecurso />
+              <SecaoSobre />
+              <Rodape />
+            </main>
+          }
+        />
+
+        {/* Página do formulário */}
+        <Route path="/cadastro-tutor" element={<FormularioTutor />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
